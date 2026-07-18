410a690d8ca709d1d58e68a9071b8f1842a0b094
hvzkn6161 <havaozk61@gmail.com>
2026-07-18 17:42:37 +0300

    Update harita.js

--- harita.js
+++ harita.js
@@ -14,3 +14,15 @@ const heartIcon = L.divIcon({
 L.marker([41.049766643187475, 39.274800166012064], {
     icon: heartIcon
 }).addTo(map).bindPopup("❤️ İlk kez buluşup oturduğumuz yer");
+
+
+
+L.marker([41.05469776379677, 39.23060642301675], {
+    icon: heartIcon
+}).addTo(map).bindPopup(`
+    <div style="text-align:center;max-width:220px;">
+        <h3>☕ İlk Kahvemiz</h3>
+        <p>📅 20 Haziran 2022</p>
+        <p>İLK ÇİÇEK :) Birlikte uzun uzun sohbet ettiğimiz yer.</p>
+    </div>
+`);
