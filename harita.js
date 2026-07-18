f7fee352dfe0a626a8558475ca666fbc54297b77
hvzkn6161 <havaozk61@gmail.com>
2026-07-18 10:46:24 +0300

    Update harita.js

--- harita.js
+++ harita.js
@@ -1,7 +1,28 @@
 const map = L.map('map').setView([39.9208, 32.8541], 13);
 
-// Harita
-
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap'
 }).addTo(map);
+
+// Kalp ikonu
+
+const heartIcon = L.divIcon({
+    className: "heart-marker",
+    html: "❤️",
+    iconSize: [40,40],
+    iconAnchor: [20,20]
+});
+
+// İlk Anı
+
+L.marker([39.9208, 32.8541], {
+    icon: heartIcon
+}).addTo(map).bindPopup(`
+    <div style="text-align:center;max-width:220px;">
+        <h3>❤️ İlk Buluşmamız</h3>
+
+        <p>📅 15 Haziran 2022</p>
+
+        <p>Hayatımızın en güzel günü...</p>
+    </div>
+`);
