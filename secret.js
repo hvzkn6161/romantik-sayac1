5118cdf3dc8fe13665f1b987c7e085f9446e8032
hvzkn6161 <havaozk61@gmail.com>
2026-07-18 18:21:48 +0300

    Update secret.js

--- secret.js
+++ secret.js
@@ -8,6 +8,19 @@ secretHeart.addEventListener("click", () => {
 
     console.log("Kalbe basıldı:", sayac);
 secretHeart.classList.add("pop");
+  const mini = document.createElement("span");
+
+mini.innerHTML = "❤️";
+
+mini.className = "mini-heart";
+
+secretHeart.appendChild(mini);
+
+setTimeout(() => {
+
+    mini.remove();
+
+},2000);  
 
 setTimeout(() => secretHeart.classList.remove("pop"), 350);
 });
