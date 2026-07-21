4bb4231a159d9035830df4161d3a667696bacfc4
hvzkn6161 <havaozk61@gmail.com>
2026-07-21 13:49:42 +0300

    Update heart.js

--- heart.js
+++ heart.js
@@ -1,5 +1,6 @@
 const heart = document.getElementById("heart");
 const counter = document.getElementById("counter");
+const particles = document.getElementById("heartParticles");
 
 let clicks = 0;
 let size = 120;
@@ -20,3 +21,24 @@ void heart.offsetWidth;
 heart.classList.add("beat");
 
 });
+function createHeart(){
+
+    const h = document.createElement("div");
+
+    h.className = "small-heart";
+
+    h.innerHTML = "💖";
+
+    const rect = heart.getBoundingClientRect();
+
+    h.style.left = (rect.left + rect.width/2 - 10 + (Math.random()*80-40)) + "px";
+
+    h.style.top = (rect.top + rect.height/2) + "px";
+
+    particles.appendChild(h);
+
+    setTimeout(() => {
+        h.remove();
+    }, 1200);
+
+}
